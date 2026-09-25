bea71253f5a6f67f2b987a96a4234468fbfd9d2a
sachinsingh83299-a11y <sachin.singh83299@gmail.com>
2026-09-25 07:51:52 +0530

    Create capacitor.config.ts

---
+++ .github/workflows/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nova.ai',
+  appName: 'Nova AI',
+  webDir: 'dist'
+};
+
+export default config;
